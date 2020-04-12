c6b6bd6fbcf48d43aabd0b2c8490b84a6421a379
LorenzoM1997 <lorenz.m97@gmail.com>
2020-04-12 19:00:53 +0200

    Initial commit

---
+++ data_collection.py
@@ -0,0 +1,14 @@
+from tf_agents.trajectories import trajectory
+
+def collect_step(environment, policy, buffer):
+  time_step = environment.current_time_step()
+  action_step = policy.action(time_step)
+  next_time_step = environment.step(action_step.action)
+  traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+  # Add trajectory to the replay buffer
+  buffer.add_batch(traj)
+
+def collect_data(env, policy, buffer, steps):
+  for _ in range(steps):
+    collect_step(env, policy, buffer)
